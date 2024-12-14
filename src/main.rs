4e7f666bfa3109480d6a439853e3e6b0d4cc2de8
Emile <despauxemile@gmail.com>
2024-12-13 21:38:29 -0500

    refac

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn main() {
 
 }
 
-fn solve_dc(resistors: &[Resistor], v_sources: &[VSource], i_sources: &[ISource]) -> Vec<f64> {
+fn create_nodes_set(resistors: &[Resistor], v_sources: &[VSource], i_sources: &[ISource]) -> BTreeSet<usize> {
     // node population generation
     let mut nodes = BTreeSet::<usize>::new();
 
@@ -53,66 +53,74 @@ fn solve_dc(resistors: &[Resistor], v_sources: &[VSource], i_sources: &[ISource]
         nodes.insert(is.1);
     }
 
-    let ref_node = nodes.first().unwrap();
+    return nodes;
+}
 
-    let nb_node_rows = nodes.len() - 1;
+fn solve_dc(resistors: &[Resistor], v_sources: &[VSource], i_sources: &[ISource]) -> Vec<f64> {
+    let mut nodes = create_nodes_set(resistors, v_sources, i_sources);
+    let ref_node = nodes.pop_first().expect("node list is empty");
+
+    let nb_nodes = nodes.len();
     let nb_vsources = v_sources.len();
 
-    let mut g_m = DMatrix::<f64>::zeros(nb_node_rows, nb_node_rows);
-    let mut b_m = DMatrix::<f64>::zeros(nb_node_rows, nb_vsources);
+    let mut g_m = DMatrix::<f64>::zeros(nb_nodes, nb_nodes);
+    let mut b_m = DMatrix::<f64>::zeros(nb_nodes, nb_vsources);
     let d_m = DMatrix::<f64>::zeros(nb_vsources, nb_vsources);
 
-    let mut z_v = DVector::<f64>::zeros(nb_node_rows + nb_vsources);
+    let mut z_v = DVector::<f64>::zeros(nb_nodes + nb_vsources);
 
     for r in resistors {
-        let Resistor(n1, n2, r_val) = r;
+        let Resistor(n1, n2, r_val) = *r;
 
-        let g: f64 = 1.0 / f64::from(*r_val);
+        let g: f64 = 1.0 / f64::from(r_val);
 
-        let n1_idx = nodes.iter().position(|n| n == n1).unwrap();
-        let n2_idx = nodes.iter().position(|n| n == n2).unwrap();
+        let n1_idx = nodes.iter().position(|&n| n == n1);
+        let n2_idx = nodes.iter().position(|&n| n == n2);
 
-        if n1 != ref_node {
-            g_m[(n1_idx-1, n1_idx-1)] += g;
+        if let Some(n1_idx) = n1_idx {
+            g_m[(n1_idx, n1_idx)] += g;
         }
-        if n2 != ref_node {
-            g_m[(n2_idx-1, n2_idx-1)] += g;
+        if let Some(n2_idx) = n2_idx {
+            g_m[(n2_idx, n2_idx)] += g;
         }
         if n1 != ref_node && n2 != ref_node {
-            g_m[(n1_idx-1, n2_idx-1)] -= g;
-            g_m[(n2_idx-1, n1_idx-1)] -= g;
+            let n1_idx = n1_idx.unwrap();
+            let n2_idx = n2_idx.unwrap();
+
+            g_m[(n1_idx, n2_idx)] -= g;
+            g_m[(n2_idx, n1_idx)] -= g;
         }
     }
 
     for (idx, v) in v_sources.iter().enumerate() {
-        let VSource(n_neg, n_pos, v_val) = v;
+        let VSource(n_neg, n_pos, v_val) = *v;
 
-        let n_neg_idx = nodes.iter().position(|n| n == n_neg).unwrap();
-        let n_pos_idx = nodes.iter().position(|n| n == n_pos).unwrap();
+        let n_neg_idx = nodes.iter().position(|&n| n == n_neg);
+        let n_pos_idx = nodes.iter().position(|&n| n == n_pos);
 
-        if n_neg != ref_node {
-            b_m[(n_neg_idx-1, idx)] = -1.0;
+        if let Some(n_neg_idx) = n_neg_idx {
+            b_m[(n_neg_idx, idx)] = -1.0;
         }
 
-        if n_pos != ref_node {
-            b_m[(n_pos_idx-1, idx)] = 1.0;
+        if let Some(n_pos_idx) = n_pos_idx {
+            b_m[(n_pos_idx, idx)] = 1.0;
         }
 
-        z_v[nb_node_rows + idx] = *v_val;
+        z_v[nb_nodes + idx] = v_val;
     }
 
     for i in i_sources {
-        let ISource(n_neg, n_pos, i_val) = i;
+        let ISource(n_neg, n_pos, i_val) = *i;
 
-        let n_neg_idx = nodes.iter().position(|n| n == n_neg).unwrap();
-        let n_pos_idx = nodes.iter().position(|n| n == n_pos).unwrap();
+        let n_neg_idx = nodes.iter().position(|&n| n == n_neg);
+        let n_pos_idx = nodes.iter().position(|&n| n == n_pos);
 
-        if n_neg != ref_node {
-            z_v[n_neg_idx-1] = -i_val;
+        if let Some(n_neg_idx) = n_neg_idx {
+            z_v[n_neg_idx] = -i_val;
         }
 
-        if n_pos != ref_node {
-            z_v[n_pos_idx-1] = *i_val;
+        if let Some(n_pos_idx) = n_pos_idx {
+            z_v[n_pos_idx] = i_val;
         }
     }
     let c_m = b_m.transpose();
@@ -120,7 +128,7 @@ fn solve_dc(resistors: &[Resistor], v_sources: &[VSource], i_sources: &[ISource]
     let a_m = nalgebra::stack![g_m, b_m; c_m, d_m];
 
     let x_m = a_m.lu().solve(&z_v).unwrap();
-    let nodes_v = x_m.as_slice()[0..nb_node_rows].to_owned();
+    let nodes_v = x_m.as_slice()[0..nb_nodes].to_owned();
 
     return nodes_v;
 }
